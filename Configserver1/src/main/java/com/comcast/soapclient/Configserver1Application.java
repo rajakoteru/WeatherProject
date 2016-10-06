a4dfc99249bfc502ce50a4806e476926e3d5192e
rajakoteru <rajakoteru@gmail.com>
2016-10-06 11:13:00 -0400

    my first commit

---
+++ Configserver1/src/main/java/com/comcast/soapclient/Configserver1Application.java
@@ -0,0 +1,14 @@
+package com.comcast.soapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Configserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configserver1Application.class, args);
+	}
+}
